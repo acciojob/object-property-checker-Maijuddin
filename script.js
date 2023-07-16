4cbd6d6be182ba8edba8b1d8d23fd62e1e1dd698
Maijuddin Mallick <124431155+Maijuddin@users.noreply.github.com>
2023-07-16 09:31:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	let keys = Object.keys(sampleObject);
+	for (var i = 0; i < keys.length; i++) {
+	  if (key == keys[i]) {
+		  return true;
+	  }
+}
+	return false;
 }
 
 // Do not change the code below
